3c60418cb8091869ca289c5f97c45a74174b2436
lightrao <lightrao@hotmail.com>
2024-11-12 13:09:19 +0800

    How to Use Container Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MyApp(),
+    const MyApp(),
   );
 }
 
@@ -13,8 +13,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            verticalDirection: VerticalDirection.down,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.00,
+                width: 100.00,
+                color: Colors.white,
+                child: const Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20.00,
+              ),
+              Container(
+                height: 100.00,
+                width: 200.00,
+                color: Colors.blue,
+                child: const Text('Container 2'),
+              ),
+              Container(
+                height: 100.00,
+                width: 300.00,
+                color: Colors.red,
+                child: const Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 10,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
